5ce826723d76da6b8e24e1523353f8e4caa73902
ezavile <ezavile@gmail.com>
2016-11-28 23:18:04 -0600

    add TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
+
+var TodoApp = require('TodoApp');
+
 
 // load app.css
 require('style!css!sass!applicationStyles');
@@ -10,7 +12,7 @@ $(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Main}>
+    <Route path="/" component={TodoApp}>
     </Route>
   </Router>,
   document.getElementById('app')
