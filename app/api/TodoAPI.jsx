ca755470f0beb62daf589277a1b77dbb40ce0cb1
Edgar Zavala <ezavile@gmail.com>
2017-03-30 00:23:25 -0600

    add filter to show only todo completed

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,14 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    return filteredTodos;
   }
 }
--- app/api/TodoAPI.test.jsx
+++ app/api/TodoAPI.test.jsx
@@ -50,4 +50,35 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [
+      {
+        id: 1,
+        text: 'Some text here',
+        completed: true,
+      },
+      {
+        id: 2,
+        text: 'Other text here',
+        completed: false,
+      },
+      {
+        id: 3,
+        text: 'More text here',
+        completed: true,
+      },
+    ];
+
+    it('should return all item if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+    it('should return non-completed todos when showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+  })
 });
